56a1fa6644e003c0ca2f4e2a5fd1648893ddbbaf
hyuckbin <rpdak159@naver.com>
2025-10-04 18:34:06 +0900

    a

--- src/App.js
+++ src/App.js
@@ -6,22 +6,19 @@ import EditUser from "./components/User/EditUser";
 import User from "./components/User/User";
 import Header from "./components/Common/Header";
 import Home from "./components/Layout/Home";
+
 function App() {
   return (
     <div className="App">
       <header className="container">
-        <div className="">
-          <Header />
-          <Routes>
-          
-            <Route path="/" element={<Home />} />
-            <Route path="/edit-user/:id" element={<EditUser />} />
-            <Route path="/user/:id" element={<User />} />
-            <Route path="/create-user" element={<CreateUser />} />
-            <Route path="/show-user" element={<ShowUser />} />
-          </Routes>
-          
-        </div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-user/:id" element={<EditUser />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="/create-user" element={<CreateUser />} />
+          <Route path="/show-user" element={<ShowUser />} />
+        </Routes>
       </header>
     </div>
   );
